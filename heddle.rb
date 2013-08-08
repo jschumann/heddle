b620610287eba532e72b3d4b2fec30fc281aa10e
James Schumann <jschumann@gmail.com>
2013-08-08 09:25:48 -0400

    reorganized to be used as a submodule

--- heddle.rb
+++ heddle.rb
@@ -2,9 +2,6 @@ module Heddle
   require 'rubygems'
   require 'data_mapper'
 
-  # A MySQL connection:
-  # DataMapper.setup(:heddle, Heddle::DatabaseConnectionString)
-
   require File.join(File.dirname(__FILE__), 'heddle/heddle_pseudonym.rb')
   require File.join(File.dirname(__FILE__), 'heddle/heddle_user.rb')
 end
