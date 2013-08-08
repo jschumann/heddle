d79b0b0b6dec3a678c820ed03de451c422b1c2c7
James Schumann <jschumann@gmail.com>
2013-08-08 09:28:49 -0400

    required two new classes in heddle loader

--- heddle.rb
+++ heddle.rb
@@ -4,5 +4,6 @@ module Heddle
 
   require File.join(File.dirname(__FILE__), 'heddle/heddle_pseudonym.rb')
   require File.join(File.dirname(__FILE__), 'heddle/heddle_user.rb')
+  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_module_progressions.rb')
+  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_modules.rb')
 end
-
