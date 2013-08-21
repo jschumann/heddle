47db11d410a7f3cd35d63ffd59b68f5f4659f37e
James Schumann <jschumann@gmail.com>
2013-08-21 15:50:04 -0400

    updated heddle.rb with new models and model names

--- heddle.rb
+++ heddle.rb
@@ -4,6 +4,7 @@ module Heddle
 
   require File.join(File.dirname(__FILE__), 'heddle/heddle_pseudonym.rb')
   require File.join(File.dirname(__FILE__), 'heddle/heddle_user.rb')
-  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_module_progressions.rb')
-  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_modules.rb')
+  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_module_progression.rb')
+  require File.join(File.dirname(__FILE__), 'heddle/heddle_context_module.rb')
+  require File.join(File.dirname(__FILE__), 'heddle/heddle_course.rb')
 end
